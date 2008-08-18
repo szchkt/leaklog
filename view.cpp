cb7cba6fb49e3a46f2e59d971604a6f127824123
Matus Tomlein <matus.tomlein@gmail.com>
2008-08-18 12:30:52 +0200

    SQL: viewAllCustomers()

--- view.cpp
+++ view.cpp
@@ -21,8 +21,53 @@
 
 void MainWindow::viewAllCustomers()
 {
-    // dict_html.value(tr("All customers")).arg()
-    // wv_main->setHtml(QString);
+    QString html; QTextStream out(&html);
+    QSqlQuery query;
+    query.setForwardOnly(true);
+    query.prepare("SELECT id, company, contact_person, address, mail, phone FROM customers ORDER BY id");
+    query.exec();
+    //query.setForwardOnly();
+    //query.prepare("");
+    //query.bindVariable();
+    //query.exec();
+    while (query.next()) {
+        out << "<tr style=\"background-color: #eee;\"><td colspan=\"2\" style=\"font-size: large; text-align: center;\"><b>" << tr("Company:") << "&nbsp;";
+        out << "<a href=\"customer:" << query.value(0).toString() << "\">" << query.value(1).toString() << "</a></b></td></tr>";
+        out << "<tr><td><table cellspacing=\"0\" cellpadding=\"4\" style=\"width:100%;\"><tr><td style=\"text-align: right; width:50%;\">" << tr("ID:") << "&nbsp;</td>";
+        out << "<td>" << query.value(0).toString() << "</td></tr>";
+        out << "<tr><td style=\"text-align: right; width:50%;\"><b>" << tr("Contact person:") << "&nbsp;</b></td>";
+        out << "<td><b>" << query.value(2).toString() << "</b></td></tr>";
+        out << "<tr><td style=\"text-align: right; width:50%;\">" << tr("Address:") << "&nbsp;</td>";
+        out << "<td>" << query.value(3).toString() << "</td></tr>";
+        out << "<tr><td style=\"text-align: right; width:50%;\">" << tr("E-mail:") << "&nbsp;</td>";
+        out << "<td>" << query.value(4).toString() << "</td></tr>";
+        out << "</table></td><td><table cellspacing=\"0\" cellpadding=\"4\" style=\"width:100%;\">";
+        out << "<tr><td style=\"text-align: right; width:50%;\">" << tr("Phone:") << "&nbsp;</td>";
+        out << "<td>" << query.value(5).toString() << "</td></tr>";
+        out << "<tr><td style=\"text-align: right; width:50%;\">" << tr("Number of circuits:") << "&nbsp;</td>";
+        out << "<td>";
+        QSqlQuery circuits;
+        circuits.setForwardOnly(true);
+        circuits.prepare("SELECT id FROM circuits WHERE parent = :parent");
+        circuits.bindValue(":parent", query.value(0).toInt());
+        circuits.exec();
+        int num_circuits = 0, num_inspections = 0;
+        while (circuits.next()) {
+            num_circuits++;
+            QSqlQuery inspections;
+            inspections.setForwardOnly(true);
+            inspections.prepare("SELECT COUNT(date) FROM inspections WHERE parent = :parent");
+            inspections.bindValue(":parent", circuits.value(0).toInt());
+            inspections.exec();
+            if (inspections.next()) { num_inspections += inspections.value(0).toInt(); }
+        }
+        out << num_circuits;
+        out << "</td></tr>";
+        out << "<tr><td style=\"text-align: right; width:50%;\">" << tr("Total number of inspections:") << "&nbsp;</td>";
+        out << "<td>" << num_inspections << "</td></tr>";
+        out << "</table></td></tr>";
+    }
+    wv_main->setHtml(dict_html.value(tr("All customers")).arg(html));
 }
 
 void MainWindow::viewCustomer(const QString &)
