a7d993973e46deb7b221d2e806ddc0ac4b1a9be0
Matus Tomlein <matus.tomlein@gmail.com>
2011-02-06 00:45:01 +0100

    added HTMLDataElement, HTMLParentElement, HTMLDiv classes. moved some functions from HTMLTable classes to HTMLParentElement

--- src/htmlbuilder.cpp
+++ src/htmlbuilder.cpp
@@ -2,106 +2,96 @@
 
 #include <QTextStream>
 
-HTMLTable::HTMLTable(const QString & args)
+HTMLParentElement::HTMLParentElement(const QString & args)
 {
     this->args = args;
 }
 
-HTMLTable::~HTMLTable()
+HTMLParentElement::~HTMLParentElement()
 {
-    for (int i = rows.count() - 1; i >= 0; --i) {
-        delete rows.takeAt(i);
+    for (int i = children.count() - 1; i >= 0; --i) {
+        delete children.takeAt(i);
     }
+}
+
+HTMLParentElement & HTMLParentElement::operator<< (const QString & str)
+{
+    children.append(new HTMLDataElement(str));
+
+    return *this;
+}
+
+HTMLDataElement::HTMLDataElement(const QString & body)
+{
+    this->body = body;
+}
+
+const QString HTMLDataElement::html()
+{
+    return body;
+}
+
+const QString HTMLParentElement::html()
+{
+    QString str;
+    QTextStream out(&str);
+
+    out << "<" << tagName() << " " << args << ">";
+    for (int i = 0; i < children.count(); ++i) {
+        out << children.at(i)->html();
+    }
+    out << "</" << tagName() <<">";
+
+    return str;
+}
+
+HTMLTable::HTMLTable(const QString & args):
+HTMLParentElement(args)
+{
+    tag_name = "table";
 }
 
 HTMLTableRow * HTMLTable::addRow(const QString & row_args)
 {
     HTMLTableRow * row = new HTMLTableRow(row_args);
-    rows.append(row);
+    children.append(row);
     return row;
 }
 
-const QString HTMLTable::html()
+HTMLTableRow::HTMLTableRow(const QString & args):
+HTMLParentElement(args)
 {
-    QString str;
-    QTextStream out(&str);
-
-    out << "<table " << args << ">";
-    for (int i = 0; i < rows.count(); ++i) {
-        out << rows.at(i)->html();
-    }
-    out << "</table>";
-
-    return str;
-}
-
-HTMLTableRow::HTMLTableRow(const QString & args)
-{
-    this->args = args;
-}
-
-HTMLTableRow::~HTMLTableRow()
-{
-    for (int i = cells.count() - 1; i >= 0; --i) {
-        delete cells.takeAt(i);
-    }
+    tag_name = "tr";
 }
 
 HTMLTableCell * HTMLTableRow::addCell(const QString & cell_args)
 {
     HTMLTableCell * cell = new HTMLTableCell(cell_args);
-    cells.append(cell);
+    children.append(cell);
     return cell;
 }
 
 HTMLHeaderTableCell * HTMLTableRow::addHeaderCell(const QString & cell_args)
 {
     HTMLHeaderTableCell * cell = new HTMLHeaderTableCell(cell_args);
-    cells.append(cell);
+    children.append(cell);
     return cell;
 }
 
-const QString HTMLTableRow::html()
+HTMLTableCell::HTMLTableCell(const QString & args):
+HTMLParentElement(args)
 {
-    QString str;
-    QTextStream out(&str);
-
-    out << "<tr " << args << ">";
-    for (int i = 0; i < cells.count(); ++i) {
-        out << cells.at(i)->html();
-    }
-    out << "</tr>";
-
-    return str;
-}
-
-HTMLTableCell::HTMLTableCell(const QString & args)
-{
-    this->args = args;
     tag_name = "td";
-}
-
-HTMLTableCell & HTMLTableCell::operator<< (const QString & str)
-{
-    body.append(str);
-
-    return *this;
-}
-
-const QString HTMLTableCell::html()
-{
-    QString str;
-    QTextStream out(&str);
-
-    out << "<" << tagName() << " " << args << ">";
-    out << body;
-    out << "</" << tagName() <<">";
-
-    return str;
 }
 
 HTMLHeaderTableCell::HTMLHeaderTableCell(const QString & args):
 HTMLTableCell(args)
 {
     tag_name = "th";
+}
+
+HTMLDiv::HTMLDiv(const QString & args):
+HTMLParentElement(args)
+{
+    tag_name = "div";
 }
--- src/htmlbuilder.h
+++ src/htmlbuilder.h
@@ -14,57 +14,67 @@ public:
     virtual const QString html() = 0;
 };
 
-class HTMLTable : public HTMLElement
+class HTMLDataElement : public HTMLElement
+{
+public:
+    HTMLDataElement(const QString &);
+
+    virtual const QString html();
+
+protected:
+    QString body;
+};
+
+class HTMLParentElement : public HTMLElement
+{
+public:
+    HTMLParentElement(const QString & = QString());
+    ~HTMLParentElement();
+
+    virtual const QString html();
+    inline const QString tagName() { return tag_name; }
+
+    HTMLParentElement & operator<<(const QString &);
+
+protected:
+    QList<HTMLElement *> children;
+    QString args;
+    QString tag_name;
+};
+
+class HTMLTable : public HTMLParentElement
 {
 public:
     HTMLTable(const QString & = QString());
-    ~HTMLTable();
 
     HTMLTableRow * addRow(const QString & = QString());
-
-    const QString html();
-
-private:
-    QList<HTMLTableRow *> rows;
-    QString args;
 };
 
-class HTMLTableRow : public HTMLElement
+class HTMLTableRow : public HTMLParentElement
 {
 public:
-    HTMLTableRow(const QString &);
-    ~HTMLTableRow();
+    HTMLTableRow(const QString & = QString());
 
     HTMLTableCell * addCell(const QString & = QString());
     HTMLHeaderTableCell * addHeaderCell(const QString & = QString());
-
-    const QString html();
-
-private:
-    QList<HTMLTableCell *> cells;
-    QString args;
 };
 
-class HTMLTableCell : public HTMLElement
+class HTMLTableCell : public HTMLParentElement
 {
 public:
-    HTMLTableCell(const QString &);
-
-    HTMLTableCell & operator<<(const QString &);
-
-    const QString html();
-    inline const QString tagName() { return tag_name; }
-
-protected:
-    QString args;
-    QString body;
-    QString tag_name;
+    HTMLTableCell(const QString & = QString());
 };
 
 class HTMLHeaderTableCell : public HTMLTableCell
 {
 public:
-    HTMLHeaderTableCell(const QString &);
+    HTMLHeaderTableCell(const QString & = QString());
+};
+
+class HTMLDiv : public HTMLParentElement
+{
+public:
+    HTMLDiv(const QString & = QString());
 };
 
 #endif // HTMLBUILDER_H
