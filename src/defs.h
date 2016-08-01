0204d249e957aa3270030ce2f97da4209ad26e21
Michal Tomlein <michal@tomlein.org>
2016-08-01 18:27:45 +0200

    Update version number to 1.1.3

--- src/defs.h
+++ src/defs.h
@@ -20,8 +20,8 @@
 #ifndef DEFS_H
 #define DEFS_H
 
-#define LEAKLOG_VERSION "1.1.2"
-#define F_LEAKLOG_VERSION 1.01021
+#define LEAKLOG_VERSION "1.1.3"
+#define F_LEAKLOG_VERSION 1.0103
 #define LEAKLOG_PREVIEW_VERSION 0
 #define DB_VERSION "0.9.9"
 #define F_DB_VERSION 0.909
